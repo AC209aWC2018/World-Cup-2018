cfb11e8eecdd8b2f611aa53fa7381063a67f4b79
linbrian <brianlin@fas.harvard.edu>
2018-11-26 16:59:30 -0500

    Added 2006

--- models/predict_test_data.py
+++ models/predict_test_data.py
@@ -10,11 +10,13 @@ def predict_test_data(test_df, columns, model):
             prediction = model.predict(test_df.iloc[i][columns].values.reshape(1, len(columns)))
             predictions.append(prediction[0])
         else:
+            #first column is home_win = -1, second column is home_win = 0, third column is home_win = 1
             probs = model.predict_proba(test_df.iloc[i][columns].values.reshape(1, len(columns)))[:, [0, 2]]
-            if probs[0][0] > probs[0][1]:
-                predictions.append(0)
-            else:
+            #check if third column probability greater than first. If it is, predict home win else predict home lose.
+            if probs[0][1] > probs[0][0]:
                 predictions.append(1)
+            else:
+                predictions.append(-1)
             
     return predictions
         
